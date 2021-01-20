5ae32b3280d94d889372336b58fcc2070bbd864f
Daniah Algudaimi <dgudaimi@gmail.com>
2021-01-20 08:48:42 +0300

    feat: modified documents
    added a new line in README, and a comment in bikeshare_2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 docs: a new branch called documentation was created, as per section 2 task C
 docs: a second edit to the README file, as per section 2
 docs: yet another edit to the README file, as per section 2
+docs: a change requested by section 2 of the assignments
--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+#this file will check user data from three different .csv files. Note: this edit is for the git project
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
